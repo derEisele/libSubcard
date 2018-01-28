d6d5d6ae845b6c08a1d885fb4cb3addadaf7c5dd
Alexander Eisele <alex.eisele99@gmail.com>
2018-01-28 16:43:58 +0100

    Fix setup

--- libsubcard/__init__.py
+++ libsubcard/__init__.py
@@ -1,3 +1,3 @@
 # coding: utf-8
-from api import *
+from .api import *
 __version__ = '0.1'
--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
   url = 'https://github.com/derEisele/libSubcard',
   download_url = 'https://github.com/derEisele/libSubcard/archive/0.1.tar.gz',
   keywords = ['api'],
+  install_requires=[
+    'requests>=1.0',
+  ],
   classifiers = [],
   )
